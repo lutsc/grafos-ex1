d88dc634f1b3a7080166e432827d871677136031
LucasSiqueiraMaia <lucas.siqmaia@gmail.com>
2026-08-04 10:30:46 -0300

    Início do 'FrontEnd'

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main ()
 {
+	int condDirigido, numGrafo;
+
+	puts("O gráfico é dirigido?\n[1] Sim\n[0] Não");
+	scanf("%d" ,&condDirigido);
+
+	printf("\nQuantos vértices terão em seu grafo: ");
+	scanf("%d", &numGrafo);
+
+	printf("Dirigido: %d, Quantidade: %d\n", condDirigido, numGrafo);
+
 	return 0;
 }
