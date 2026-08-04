3854d62aa07fe9b3a410682fd354bc923e5c338b
MelkiorMisiak <misiakmelkior@gmail.com>
2026-08-04 11:04:32 -0300

    Makefile atualizado e compile_commands gerado

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
-%:%.c
-	gcc $^ -o $@
+CFLAGS += -I./include
+
+OBJS = lista_encadeada.o
+
+all:
+	gcc -c src/lista_encadeada.c -o build/lista_encadeada.o
+	gcc $(CFLAGS) src/main.c build/$(OBJS) -o bin/main
+
+clean:
+	rm -rf build/*
+	rm -rf bin/*
+
--- include/lista_encadeada.h
+++ include/lista_encadeada.h
@@ -10,17 +10,17 @@ struct Node
 /*
  * Insere dado no fim da lista, retorna 0 em sucesso e 1 em outros casos.
  */
-int insereListaFim(Node * root, void * data);
+int insereListaFim(struct Node * root, void * data);
 
 /*
  * Remove o dado no fim da lista, retorna 0 em sucesso e 1 em outros casos.
  */
-int removeListaFim(Node * root);
+int removeListaFim(struct Node * root);
 
 /*
  *  Busca um dado na lista e retorna o ponteiro do Nodo em *ret, a função
  *  retorna 0 caso encontre o dado e 1 caso contrário.
  */
-int buscaLista(Node * root, void * data, Node * ret);
+int buscaLista(struct Node * root, void * data, struct Node * ret);
 
 #endif
