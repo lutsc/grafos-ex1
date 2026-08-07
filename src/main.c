42a6ba630eb362abc0d67066f06347e8f10f70ff
lutsc <170535336+lutsc@users.noreply.github.com>
2026-08-06 23:31:57 -0300

    Adicionado testes para verificar funcionamento da alocação de conexões

--- src/main.c
+++ src/main.c
@@ -6,39 +6,72 @@ int main ()
 {
 	int condDirigido, numGrafo;
 
-	printf("\n");
-	puts("O gráfico é dirigido?\n[1] Sim\n[0] Não");
+	puts("\nO gráfico é dirigido?\n[1] Sim\n[0] Não");
 	scanf("%d" ,&condDirigido);
+	if(condDirigido < 0 || condDirigido > 1){
+		printf("Opção inválida.\n");
+		return 1;
+	}
 
 	printf("\nQuantos vértices terão em seu grafo: ");
 	scanf("%d", &numGrafo);
+	if(numGrafo <= 0){
+		printf("Opção inválida.\n");
+		return 1;
+	}
 
 	printf("Dirigido: %d, Quantidade: %d\n", condDirigido, numGrafo);
 	
 	// Criando vetor dinâmico e alocando espaço
-	struct Node *array = malloc(numGrafo * sizeof(struct Node));
-	
-	// Alocando valores teste
+	struct Node * array = (struct Node *)calloc(numGrafo, sizeof(struct Node));
+	if(array == NULL){
+		printf("\n Erro ao alocar memória para o vetor dinâmico.");
+		return 1;
+	}
+
+	// Alocando valores temp
 	for(size_t i = 0; i < (size_t)numGrafo; i++){
-		int *valor = malloc(sizeof(int));
-		*valor = i+1;
+		int * valor; 
+		valor = (int *)malloc(sizeof(int));
+		* valor = i+1;
 		array[i].data = valor;
 	}
 
-	// Testando se valores 
+	// Testando se elementos do vetor dinâmico estão funcionais
 	printf("\n");
 	for(size_t i = 0; i < numGrafo; i++){
-		printf("%d\n", *(int *)array[i].data);
+		printf("Elemento %d ativo.\n", *(int *)array[i].data);
 	}
 	printf("\n");
-	
+
+	// Valores teste para representar as conexões das vértices
+	char conexoes1[] = {'A', 'B', 'C'};
+	char conexoes2[] = {'D', 'E', 'F'};
+
+	// Alocando conexões
+	for (size_t i = 0; i < 3; i++) {
+		char * temp = (char *)calloc(1, sizeof(char));
+		* temp = conexoes1[i];
+		insereListaFim(&(array[0].next), temp);
+		temp = (char *)calloc(1, sizeof(char));
+		* temp = conexoes2[i];
+		insereListaFim(&(array[1].next), temp);
+	}
+
+	// Verificando se elementos tiveram suas conexões alocadas corretamente
+	printf("Conexões do 1° elemento: ");
+	imprimirLista(array[0].next, imprimeChar);
+	printf("\n");
+	printf("Conexões do 2° elemento: ");
+	imprimirLista(array[1].next, imprimeChar);
+	printf("\n");
+
 	// Liberando dados e vetor
 	for(size_t i = 0; i < numGrafo; i++){
+		liberarLista(array[i].next);
 		free(array[i].data);
 		array[i].data = NULL;
 	}
-	free(array);
-	array = NULL;
-
+	
 	return 0;
 }
