4c6c80742048bf013a1f9356655fc7073d4fecea
lutsc <170535336+lutsc@users.noreply.github.com>
2026-09-03 19:05:57 -0300

    Ajustado dependências e arranjo das funções, renomeado variáveis e corrigido pequenos bugs, adicionado retorno dos valores do nó removido em removeListaFim

--- src/lista_encadeada.c
+++ src/lista_encadeada.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lista_encadeada.h"
 
-int32_t criarNode(struct Node ** node, void * data, size_t id) {
+int32_t iniciarLista(struct List * list, uint32_t id) {
+	if (list == NULL)
+		return 1;
+ 
+	list->id = id;
+	list->nodeQtd = 0;
+	list->head = NULL;
+	return 0;
+}
+
+int32_t criarNode(struct Node ** node, uint32_t data, uint32_t id) {
 	struct Node * newNode = malloc(sizeof(struct Node));
 	if (newNode == NULL)
 		return 1;
 
-	newNode ->id = id;
+	newNode->id = id;
 	newNode->data = data;
 	newNode->next = NULL;
 
@@ -15,102 +23,85 @@ int32_t criarNode(struct Node ** node, void * data, size_t id) {
 	return 0;
 }
 
-int32_t insereListaFim(struct List * root, void * data, size_t id) {
-	if (root == NULL)
+int32_t insereListaFim(struct List * list, struct Node ** node) {
+	if (list == NULL)
 		return 1;
 
-	struct Node * newNode = NULL;
-	if (criarNode(&newNode, data, id) != 0)
-		return 1;
-
-	if (root->head == NULL) {
-		root->head = newNode;
+	if (list->head == NULL) {
+		list->head = * node;
 	} else {
-		struct Node * t = root->head;
-		while (t->next != NULL)
-			t = t->next;
-		t->next = newNode;
+		struct Node * temp = list->head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = * node;
 	}
 
-	root->edgesQtd++;
+	list->nodeQtd++;
 	
 	return 0;
 }
 
-int32_t removeListaFim(struct List * root) { //TODO: Retornar node removido
-	if (root == NULL || root->head == NULL)
+int32_t removeListaFim(struct List * list, struct Node ** ret) {
+	if (list == NULL || list->head == NULL)
 		return 1;
 
-	struct Node * t1 = root->head;
-	struct Node * t2 = t1->next;
+	struct Node * temp1 = list->head;
+	struct Node * temp2 = temp1->next;
 
-	if (t2 == NULL) {
-		root->head = NULL;
-		free(t1);
+	if (temp2 == NULL) {
+		(*ret)->data = temp1->data;
+		(*ret)->id = temp1->id;
+		list->head = NULL;
+		free(temp1);
+		list->nodeQtd--;
 		return 0;
 	}
 
-	while (t2->next != NULL) {
-		t1 = t1->next;
-		t2 = t2->next;
+	while (temp2->next != NULL) {
+		temp1 = temp1->next;
+		temp2 = temp2->next;
 	}
 
-	t1->next = NULL;
-	free(t2->data);
-	free(t2);
-	root->edgesQtd--;
+	(*ret)->data = temp2->data;
+	(*ret)->id = temp2->id;
+
+	temp1->next = NULL;
+	free(temp2);
+	list->nodeQtd--;
 
 	return 0;
 }
 
-int32_t buscaLista(struct List * root, void * data, struct Node ** ret) {
-	if (root == NULL)
+int32_t buscaLista(struct List * list, uint32_t id, struct Node ** ret) {
+	if (list == NULL)
 		return 1;
 
-	struct Node * atual = root->head;
-	while (atual != NULL) {
-		if (atual->data == data) {
-			*ret = atual;
+	struct Node * temp = list->head;
+	while (temp != NULL) {
+		if (temp->id == id) {
+			*ret = temp;
 			return 0;
 		}
-		atual = atual->next;
+		temp = temp->next;
 	}
 	return 1;
 }
 
-void imprimirLista(struct List * root, void (* printFunction)(void *)) {
-	struct Node * atual = root->head;
-	while (atual != NULL) {
-		printFunction(atual->data);
-		atual = atual->next;
-	}
-}
-
-void liberarLista(struct List * root) {
-	struct Node * atual = root->head;
+void liberarLista(struct List * list) {
+	struct Node * atual = list->head;
 	while (atual != NULL) {
 		struct Node * prox = atual->next;
-		free(atual->data);
-		atual->data = NULL;
 		free(atual);
 		atual = prox;
 	}
-	root->head = NULL;
-	root->edgesQtd = 0;
+	list->head = NULL;
+	list->nodeQtd = 0;
 }
 
-
-int32_t iniciarLista(struct List * root) {
-	if (root == NULL)
-		return 1;
- 
-	root->id = 0;
-	root->edgesQtd = 0;
-	root->head = NULL;
-	return 0;
-}
-
-
-void imprimeChar(void * data) {
-	printf("%c, ", *(char *)data);
+void imprimirLista(struct List * list) {
+	struct Node * atual = list->head;
+	while (atual != NULL) {
+		printf("%d ", atual->data);
+		atual = atual->next;
+	}
 }
