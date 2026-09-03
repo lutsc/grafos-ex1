aef443feef2ff0390d4672bfd68fecf56a95e262
lutsc <170535336+lutsc@users.noreply.github.com>
2026-09-03 19:05:57 -0300

    Ajustado dependências e arranjo das funções, renomeado variáveis e corrigido pequenos bugs, adicionado retorno dos valores do nó removido em removeListaFim

--- include/lista_encadeada.h
+++ include/lista_encadeada.h
@@ -1,59 +1,55 @@
 #ifndef H_LISTA_ENCADEADA
 #define H_LISTA_ENCADEADA 1
-#include <stdbool.h>
+
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct Node {
-	size_t id;
-	void * data; //FIX: Matar (talvez)
+	uint32_t id;
+	uint32_t data;
 	struct Node * next;
 };
 
 struct List {
-	size_t id; 
-	size_t edgesQtd;
+	uint32_t id;
+	uint32_t nodeQtd;
 	struct Node * head;
 };
 
 /*
- * Aloca memória para um node. Retorna 0 em sucesso e 1 em falha de alocação.
+ * Inicializa a List
  */
-int32_t criarNode(struct Node ** node, void * data, size_t id);
+int32_t iniciarLista(struct List * list, uint32_t id);
+
+/*
+ * Aloca memória para um node, retorna 0 em sucesso e 1 em falha de alocação.
+ */
+int32_t criarNode(struct Node ** node, uint32_t data, uint32_t id);
 
 /*
  * Insere dado no fim da lista, retorna 0 em sucesso e 1 em outros casos.
  */
-int32_t insereListaFim(struct List * root, void * data, size_t id);
+int32_t insereListaFim(struct List * list, struct Node ** node);
 
 /*
  * Remove o dado no fim da lista, retorna 0 em sucesso e 1 em outros casos.
  */
-int32_t removeListaFim(struct List * root);
+int32_t removeListaFim(struct List * list, struct Node ** ret);
 
 /*
- *  Busca um dado na lista e retorna o ponteiro do Nodo em *ret, a função
- *  retorna 0 caso encontre o dado e 1 caso contrário.
+ * Busca um dado na lista e retorna o ponteiro do Nodo em *ret, a função retorna 0 caso encontre o dado e 1 caso contrário.
  */
-int32_t buscaLista(struct List * root, void * data, struct Node ** ret); //TODO: Adicionar função para buscar usando id
-
-/*
- *  Imprime a lista encadeada
- */
-void imprimirLista(struct List * root, void (* printFunction)(void *));
+int32_t buscaLista(struct List * list, uint32_t data, struct Node ** ret); //TODO: Adicionar função para buscar usando id
 
 /*
  * Libera memória de todos os nós de uma lista encadeada a partir da raiz
  */
-void liberarLista(struct List * root);
+void liberarLista(struct List * list);
 
 /*
- * Inicializa a List
+ * Imprime a lista encadeada
  */
-int32_t iniciarLista(struct List * root);
-
-/*
- * Imprime dados dos nós do tipo void * em tipo char
- */
-void imprimeChar(void * data);
+void imprimirLista(struct List * list);
 
 #endif
