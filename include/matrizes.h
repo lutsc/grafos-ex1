f93d512dc1bead18eaa76cdf2405fa96a093ab83
lutsc <170535336+lutsc@users.noreply.github.com>
2026-09-06 22:44:53 -0300

    Corrigido tipo do retorno da função

--- include/matrizes.h
+++ include/matrizes.h
@@ -2,13 +2,14 @@
 #define H_MATRIZES 1
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 /*
  * Liga um nó a outro na matriz de adjacência
 */
-int32_t inserirNaMatrizAdjacente(bool ** mat, uint32_t tamanhoMatriz, uint32_t no1, uint32_t no2);
+uint32_t inserirNaMatrizAdjacente(bool ** mat, uint32_t tamanhoMatriz, uint32_t no1, uint32_t no2);
 
 uint32_t multiplicarMatrizes(bool ** mat1, bool ** mat2, bool ** matRet, uint32_t tam);
 
