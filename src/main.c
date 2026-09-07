2f77609477bcef3b14c744997107d77396f7d9e9
LucasSiqueiraMaia <lucas.siqmaia@gmail.com>
2026-09-06 21:09:08 -0300

    atualização do main.c, necessário implementação das funções bfs, dfs, conectividade, ftd e ftdi

--- src/main.c
+++ src/main.c
@@ -2,126 +2,225 @@
 #include <stdlib.h>
 #include "lista_encadeada.h"
 #include "grafos.h"
+#include "matrizes.h"
+
+// void limparTela() {
+//     #if defined(_WIN32) || defined(_WIN64)
+//         system("cls");
+//     #else
+//         system("clear");
+//     #endif
+// }
+
+static void menu(void) {
+	printf("\n===== MENU =====\n");
+	printf("[1]  Criar novo grafo\n");
+	printf("[2]  Inserir vértice\n");
+	printf("[3]  Remover vértice\n");
+	printf("[4]  Inserir aresta/arco\n");
+	printf("[5]  Remover aresta\n");
+	printf("[6]  Mostrar grafo (lista e matriz de adjacência)\n");
+	printf("[7]  Percorrer em profundidade (DFS)  (em breve)\n");
+	printf("[8]  Percorrer em largura (BFS)       (em breve)\n");
+	printf("[9]  Fecho transitivo direto de um vértice   (em breve)\n");
+	printf("[10] Fecho transitivo inverso de um vértice  (em breve)\n");
+	printf("[11] Verificar conexidade / componentes fortemente conexas (em breve)\n");
+	printf("[0]  Sair\n");
+	printf("Escolha: ");
+}
 
 int main(void) {
 	struct Graph graph;
-	bool ** matriz = NULL;
-	size_t i, j;
+	bool existeGrafo = false;
+	int opcao;
 
-	// 1. Direcionado ou não
-	puts("\nO grafo é dirigido?\n[1] Sim\n[0] Não");
-	if (scanf("%zu", &graph.directed) != 1 || (graph.directed > 1)) {
-		printf("Opção inválida.\n");
-		return 1;
-	}
+	do {
 
-	// 2. Quantidade de vértices
-	printf("\nQuantos vértices terão em seu grafo (máx. 26): ");
-	if (scanf("%zu", &graph.verticesQtd) != 1 || graph.verticesQtd == 0 || graph.verticesQtd > 26) {
-		printf("Opção inválida.\n");
-		return 1;
-	}
+		menu();
 
-	printf("Dirigido: %zu, Quantidade: %zu\n", graph.directed, graph.verticesQtd);
-
-	// Vetor de listas (lista de adjacência), uma struct List por vértice
-	if (iniciarGrafo(&graph, (int)graph.verticesQtd) != 0)
-		return 1;
-
-	// Matriz de adjacência
-	if (gerarMatrizAdjacente(&matriz, (uint32_t)graph.verticesQtd) != 0) {
-		printf("Erro ao gerar matriz de adjacência.\n");
-		free(graph.array);
-		return 1;
-	}
-
-	// 3. Arestas/arcos
-	size_t qtdArestas;
-	printf("\nOs vértices são identificados pelas letras A, B, C, ...\n");
-	printf("Quantas arestas/arcos terá o grafo: ");
-	if (scanf("%zu", &qtdArestas) != 1) {
-		printf("Opção inválida.\n");
-		return 1;
-	}
-
-	for (i = 0; i < qtdArestas; i++) {
-		size_t origem, destino;
-
-		printf("\nAresta %zu:\n", i + 1);
-		printf("Vértice de origem (0 a %zu): ", graph.verticesQtd - 1);
-		scanf("%zu", &origem);
-		printf("Vértice de destino (0 a %zu): ", graph.verticesQtd - 1);
-		scanf("%zu", &destino);
-
-		if (origem >= graph.verticesQtd || destino >= graph.verticesQtd) {
-			printf("Vértice inválido, aresta ignorada.\n");
+		if (scanf("%d", &opcao) != 1) {
+			printf("Entrada inválida.\n");
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+			if (c == EOF)
+				break;
+			continue;
+		}
+ 
+		if (opcao != 1 && opcao != 0 && !existeGrafo) {
+			printf("Crie um grafo primeiro (opção 1).\n");
 			continue;
 		}
 
-		// Insere na matriz de adjacência
-		if (inserirNaMatrizAdjacente(matriz, (uint32_t)graph.verticesQtd, (uint32_t)origem, (uint32_t)destino) == 1)
-			printf("Aviso: aresta já existia na matriz.\n");
+		switch (opcao) {
+			case 1: {
+				if (existeGrafo)
+					liberaGrafo(&graph);
+ 
+				uint32_t dirigido, vertices;
+				printf("O grafo é dirigido? [1] Sim [0] Não: ");
+				if (scanf("%u", &dirigido) != 1 || dirigido > 1) {
+					printf("Opção inválida.\n");
+					existeGrafo = false;
+					break;
+				}
+ 
+				printf("Quantidade de vértices: ");
+				if (scanf("%u", &vertices) != 1 || vertices == 0) {
+					printf("Opção inválida.\n");
+					existeGrafo = false;
+					break;
+				}
+ 
+				if (iniciarGrafo(&graph, vertices, (bool)dirigido) != 0) {
+					printf("Erro ao criar o grafo.\n");
+					existeGrafo = false;
+				} else {
+					const char * tipo;
+					if (dirigido)
+						tipo = "dirigido";
+					else
+						tipo = "não dirigido";
+ 
+					printf("Grafo criado com %u vértices (%s).\n", vertices, tipo);
+					existeGrafo = true;
+				}
+			}
 
-		// Insere na lista de adjacência (rótulo do destino como letra)
-		char * rotulo = malloc(sizeof(char));
-		*rotulo = (char)('A' + destino);
-		insereListaFim(&graph.array[origem], rotulo);
+			case 2: {
+				if (inserirVertice(&graph) == 0)
+					printf("Vértice %u inserido.\n", graph.verticesQtd);
+				else
+					printf("Erro ao inserir vértice.\n");
+				break;
+			}
 
-		// Se não for dirigido, insere também o caminho inverso
-		if (graph.directed == 0) {
-			inserirNaMatrizAdjacente(matriz, (uint32_t)graph.verticesQtd, (uint32_t)destino, (uint32_t)origem);
+			case 3: {
+				uint32_t v;
+				printf("Vértice a remover (1 a %u): ", graph.verticesQtd);
+				scanf("%u", &v);
+				if (removerVertice(&graph, v) == 0)
+				    printf("Vértice %u removido.\n", v);
+				else
+				    printf("Vértice inválido.\n");
+				
+				break;
+			}
 
-			char * rotulo2 = malloc(sizeof(char));
-			*rotulo2 = (char)('A' + origem);
-			insereListaFim(&graph.array[destino], rotulo2);
+			case 4: {
+				uint32_t origem, destino;
+				printf("Vértice de origem (1 a %u): ", graph.verticesQtd);
+				scanf("%u", &origem);
+				printf("Vértice de destino (1 a %u): ", graph.verticesQtd);
+				scanf("%u", &destino);
+
+				if (inserirAresta(&graph, origem, destino) == 0)
+					printf("Aresta %u -> %u inserida.\n", origem, destino);
+				else
+					printf("Erro ao inserir aresta (vértice inválido ou já existente).\n");
+				break;
+			}
+
+			case 5: {
+				uint32_t origem, destino;
+				printf("Vértice de origem (1 a %u): ", graph.verticesQtd);
+				scanf("%u", &origem);
+				printf("Vértice de destino (1 a %u): ", graph.verticesQtd);
+				scanf("%u", &destino);
+				if (removerAresta(&graph, origem, destino) == 0)
+				    printf("Aresta %u -> %u removida.\n", origem, destino);
+				else
+				    printf("Aresta não encontrada ou vértice inválido.\n");
+				
+				break;
+			}
+
+			case 6: {
+				// TODO: Necessário a implantação do mostrar grafo.
+				// mostrarGrafo(&graph);
+				// break;
+			}
+
+			case 7: {
+				/*
+				 * TODO: depende de dfs(struct Graph *, uint32_t)
+				 *
+				 * uint32_t v;
+				 * printf("Vértice inicial (1 a %u): ", graph.verticesQtd);
+				 * scanf("%u", &v);
+				 * printf("DFS a partir de %u: ", v);
+				 * if (dfs(&graph, v) != 0)
+				 *     printf("\nVértice inválido.\n");
+				 */
+				break;
+			}
+
+			case 8: {
+				/*
+				 * TODO: depende de bfs(struct Graph *, uint32_t)
+				 *
+				 * uint32_t v;
+				 * printf("Vértice inicial (1 a %u): ", graph.verticesQtd);
+				 * scanf("%u", &v);
+				 * printf("BFS a partir de %u: ", v);
+				 * if (bfs(&graph, v) != 0)
+				 *     printf("\nVértice inválido.\n");
+				 */
+				break;
+			}
+
+			case 9: {
+				/*
+				 * TODO: depende de fechoTransitivoDireto(struct Graph *, uint32_t),
+				 *
+				 * uint32_t v;
+				 * printf("Vértice (1 a %u): ", graph.verticesQtd);
+				 * scanf("%u", &v);
+				 * if (fechoTransitivoDireto(&graph, v) != 0)
+				 *     printf("Vértice inválido.\n");
+				 */
+				break;
+			}
+
+			case 10: {
+				/*
+				 * TODO: depende de fechoTransitivoInverso(struct Graph *, uint32_t),
+				 *
+				 * uint32_t v;
+				 * printf("Vértice (1 a %u): ", graph.verticesQtd);
+				 * scanf("%u", &v);
+				 * if (fechoTransitivoInverso(&graph, v) != 0)
+				 *     printf("Vértice inválido.\n");
+				 */
+				break;
+			}
+
+			case 11: {
+				/*
+				 * TODO: depende de grafoConexo(struct Graph *)
+				 *
+				 * if (grafoConexo(&graph)) {
+				 *     printf("O grafo é conexo.\n");
+				 * } else {
+				 *     printf("O grafo NÃO é conexo. Subgrafos fortemente conexos máximos:\n");
+				 *     componentesFortementeConexas(&graph);
+				 * }
+				 */
+				break;
+			}
+
+			case 0:
+				printf("Encerrando...\n");
+				break;
+
+			default:
+				printf("Opção inválida.\n");
+
 		}
-	}
+	} while (opcao != 0);
 
-	// 4. Mostrar matriz de adjacência
-	printf("\n===== Matriz de Adjacência =====\n\n   ");
-	for (j = 0; j < graph.verticesQtd; j++)
-		printf("%c ", (char)('A' + j));
-	printf("\n");
-
-	for (i = 0; i < graph.verticesQtd; i++) {
-		printf("%c: ", (char)('A' + i));
-		for (j = 0; j < graph.verticesQtd; j++)
-			printf("%d ", matriz[i][j]);
-		printf("\n");
-	}
-
-	// 5. Mostrar lista de adjacência (reaproveitando imprimirLista + imprimeChar)
-	printf("\n===== Lista de Adjacência =====\n");
-	for (i = 0; i < graph.verticesQtd; i++) {
-		printf("%c: ", (char)('A' + i));
-		imprimirLista(&graph.array[i], imprimeChar);
-		printf("\n");
-	}
-
-	// 6. Vértice inicial para os percursos
-	size_t inicio;
-	printf("\nVértice inicial para DFS e BFS (0 a %zu): ", graph.verticesQtd - 1);
-	if (scanf("%zu", &inicio) != 1 || inicio >= graph.verticesQtd) {
-		printf("Vértice inválido, usando o vértice 0 como início.\n");
-		inicio = 0;
-	}
-
-	// ===== DFS (busca em profundidade) =====
-	printf("\n===== DFS a partir do vértice %c =====\n", (char)('A' + inicio));
-	dfs(matriz, (uint32_t)graph.verticesQtd, (uint32_t)inicio);
-
-	// ===== BFS (busca em largura) =====
-	printf("\n===== BFS a partir do vértice %c =====\n", (char)('A' + inicio));
-	bfs(matriz, (uint32_t)graph.verticesQtd, (uint32_t)inicio);
-
-	// Liberação de memória
-	for (i = 0; i < graph.verticesQtd; i++)
-		liberarLista(&graph.array[i]);
-	free(graph.array);
-
-	for (i = 0; i < graph.verticesQtd; i++)
-		free(matriz[i]);
-	free(matriz);
+	if (existeGrafo)
+		liberaGrafo(&graph);
 
 	return 0;
 }
