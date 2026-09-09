ead8af6809df99840d828ece1b64656fc37c6b53
MelkiorMisiak <misiakmelkior@gmail.com>
2026-09-08 23:17:53 -0300

    Direcionalidade parcialmente implementada

--- src/main.c
+++ src/main.c
@@ -21,9 +21,66 @@
 #define CIRCLE_RADIUS 15
 #define PADDING 60
 
+#define ARROW_ANGLE PI/6
+#define ARROW_WING 20
+
 #define FONT_SIZE CIRCLE_RADIUS/5
 
 #define V_COLOR PURPLE
+
+#include <stdio.h>
+#include <math.h>
+
+
+Vector2 concentricPointStop(Vector2 a, Vector2 b, float r) {
+	double D = sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+
+	Vector2 c;
+	if (D == 0.0) {
+		c.x = 0;
+		c.y = 0;
+		return c;
+	}
+
+	if (D <= r)
+	  c = a;
+
+	double d = D - r;
+	double t = d / D;
+
+	c.x = a.x + t * (b.x - a.x);
+	c.y = a.y + t * (b.y - a.y);
+
+	return c;
+}
+
+void DrawArrow(Vector2 pos1, Vector2 pos2, float angle, float wing_size)
+{
+	float deltaX = pos2.x - pos1.x;
+	float deltaY = pos2.y - pos1.y;
+	float Length = sqrt(pow(deltaX,2) + pow(deltaY,2));;
+
+	if(Length == 0)
+		return;
+
+	float uX = -deltaX/Length;
+	float uY = -deltaY/Length;
+
+	double bX = uX * wing_size; 
+	double bY = uY * wing_size; 
+
+	Vector2 wing1;
+	wing1.x = bX * cos(angle)- bY * sin(angle) + pos2.x;
+	wing1.y = bX * sin(angle)+ bY * cos(angle) + pos2.y;
+
+	Vector2 wing2;
+	wing2.x = bX * cos(-angle)- bY * sin(-angle) + pos2.x;
+	wing2.y = bX * sin(-angle)+ bY * cos(-angle) + pos2.y;
+
+	DrawLineV(pos1, pos2, BLACK);
+	DrawLineV(pos2, wing1, BLACK);
+	DrawLineV(pos2, wing2, BLACK);
+}
 
 void mostrarGrafo(struct Graph * graph) {
     if (graph == NULL || graph->array == NULL) {
@@ -115,7 +172,7 @@ int main()
 
 	struct Graph graph;
 	graph.verticesQtd = 0;
-	graph.directed = 0;
+	graph.directed = 1;
 
 	iniciarGrafo(&graph, graph.verticesQtd, graph.directed);
 
@@ -158,7 +215,9 @@ int main()
 			tempNode = graph.array[i].head;
 			while(tempNode != NULL)
 			{
-				DrawLineV(graph.array[i].pos, graph.array[tempNode->id-1].pos, BLACK);
+				// DrawLineV(graph.array[i].pos, graph.array[tempNode->id-1].pos, BLACK);
+				DrawArrow(concentricPointStop(graph.array[tempNode->id-1].pos, graph.array[i].pos, CIRCLE_RADIUS), concentricPointStop(graph.array[i].pos, graph.array[tempNode->id-1].pos, CIRCLE_RADIUS), ARROW_ANGLE, ARROW_WING);
+
 				tempNode = tempNode->next;
 			}
 			if(CheckCollisionPointCircle(GetMousePosition(), graph.array[i].pos, CIRCLE_RADIUS))
